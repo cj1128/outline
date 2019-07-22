f77500271aa0870d51e177e33d4fb998114d321c
CJ <cjting1128@gmail.com>
2019-07-22 17:50:29 +0800

    Modify styles and add doc prefix to uploaded assets

--- app/utils/uploadFile.js
+++ app/utils/uploadFile.js
@@ -14,7 +14,7 @@ export const uploadFile = async (
   formData.append('name', file.name || option.name);
   formData.append('file', file);
 
-  const response = await client.post('/users.upload', formData, { contentTypeDisabled: true });
+  const response = await client.post('/users.upload', formData);
 
   invariant(response, 'Response should be available');
 
--- shared/components/TeamLogo.js
+++ shared/components/TeamLogo.js
@@ -5,8 +5,6 @@ const TeamLogo = styled.img`
   width: 38px;
   height: 38px;
   border-radius: 4px;
-  background: ${props => props.theme.white};
-  border: 1px solid ${props => props.theme.divider};
 `;
 
 export default TeamLogo;
